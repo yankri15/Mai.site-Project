f4462492865c2ad7f3821cb3d7ebde9b727dcfc7
Yoel Abecassis <yoe1997@gmail.com>
2022-03-23 15:37:53 +0200

    Created the register page

---
+++ src/Pages/Register_Screen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default () => (
+    <View>
+        <Text>This is the regiester screen</Text>
+    </View>
+)
