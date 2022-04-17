40035c1bcb158eb4dc6b700b6ce1b31e94b511ec
Yoel Abecassis <yoe1997@gmail.com>
2022-04-17 12:17:25 +0300

    Updated 'Screens' name

--- src/Pages/Register_Screen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default () => (
-    <View>
-        <Text>This is the regiester screen</Text>
-    </View>
-)
