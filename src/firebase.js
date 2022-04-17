039e7dd929f8bff061a37f0935c79f13bf6d82f0
Yoel Abecassis <yoe1997@gmail.com>
2022-04-17 12:17:58 +0300

    Updated irebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,13 +13,15 @@ const firebaseConfig = {
   projectId: "mai-app-jlm",
   storageBucket: "mai-app-jlm.appspot.com",
   messagingSenderId: "941253016294",
-  appId: "1:941253016294:web:4e210dfe94268f7d1c9927"
+  appId: "1:941253016294:web:4e210dfe94268f7d1c9927",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const firestore = getFirestore()
-// const db = firebase.firestore();
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
-// export default { firebase, db };
+export const auth = getAuth(app);
+
+export const firestore = getFirestore();
