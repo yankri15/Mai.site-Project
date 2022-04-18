734817598f95648fd9b8ef1de20ed53f567798e6
Navon Sahalo <navonsahalo@gmail.com>
2022-04-18 10:48:52 +0300

    Refactor screens

---
+++ src/Screens/GuestScreens/GuestHomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GuestHomeScreen = () => {
+    return (
+        <View>
+            <Text>GuestHomeScreen</Text>
+        </View>
+    )
+}
+
+export default GuestHomeScreen
