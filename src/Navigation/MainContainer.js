440f7953f5c8f3a62ab48f2c3a8191551e0ce890
Yoel Abecassis <yoe1997@gmail.com>
2022-04-19 13:45:34 +0300

    renamed bars

--- src/Navigation/MainContainer.js
+++ src/Navigation/MainContainer.js
@@ -1,40 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useState } from 'react/cjs/react.production.min';
+import { useState } from "react/cjs/react.production.min";
 import AuthProvider, { useAuth } from "../AuthProvider/AuthProvider";
-import FeedScreen from "../Screens/UserScreens/FeedScreen"
-import ProfileScreen from "../Screens/UserScreens/ProfileScreen"
-import RegisterScreen from '../Screens/AuthScreens/RegisterScreen';
-import LoginScreen from '../Screens/AuthScreens/LoginScreen';
-import Navbar from '../API/Navbar';
+import FeedScreen from "../Screens/UserScreens/FeedScreen";
+import ProfileScreen from "../Screens/UserScreens/ProfileScreen";
+import RegisterScreen from "../Screens/AuthScreens/RegisterScreen";
+import LoginScreen from "../Screens/AuthScreens/LoginScreen";
+import UserNavbar from "../API/UserNavbar";
 
 //
 const Stack = createNativeStackNavigator();
 
 const MainContainer = () => {
+  const { currentUser } = useAuth();
 
-    const { currentUser } = useAuth();
+  if (currentUser) {
+    return <UserNavbar />;
+  } else {
+    return (
+      <AuthProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthProvider>
+    );
+  }
+};
 
-    if (currentUser) {
-        return (
-            <Navbar />
-        )
-    }
-    else {
-        return (
-            <AuthProvider>
-                <NavigationContainer>
-                    <Stack.Navigator screenOptions={{ headerShown: false }}>
-                        <Stack.Screen name="Login" component={LoginScreen} />
-                        <Stack.Screen name="Register" component={RegisterScreen} />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </AuthProvider>
-        )
-    }
-
-}
-
-export default MainContainer
+export default MainContainer;
