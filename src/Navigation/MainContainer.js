9261dcdcc0382b9f279d8ede5a0a3c5026675c8d
AvivEldad <avivelded@gmail.com>
2022-04-25 17:05:32 +0300

    forgot password screen 00

--- src/Navigation/MainContainer.js
+++ src/Navigation/MainContainer.js
@@ -5,6 +5,7 @@ import { useAuth } from "../AuthProvider/AuthProvider";
 import RegisterScreen from "../Screens/AuthScreens/RegisterScreen";
 import LoginScreen from "../Screens/AuthScreens/LoginScreen";
 import HamburgerNav from "../API/HamburgerNav";
+import ForgotPasswordScreen from "../Screens/AuthScreens/ForgotPasswordScreen";
 // import UserNavbar from "../API/UserNavbar";
 
 const Stack = createStackNavigator();
@@ -22,6 +23,7 @@ const MainContainer = () => {
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="Register" component={RegisterScreen} />
+                <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
             </Stack.Navigator>
         );
     }
