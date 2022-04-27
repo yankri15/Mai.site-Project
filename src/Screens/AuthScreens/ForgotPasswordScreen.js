f8796ed7ca7bb791f61147a7aae300bab5cf9cf5
Yoel Abecassis <yoe1997@gmail.com>
2022-04-27 12:56:47 +0300

    onClick to onPress

--- src/Screens/AuthScreens/ForgotPasswordScreen.js
+++ src/Screens/AuthScreens/ForgotPasswordScreen.js
@@ -1,60 +1,57 @@
 import React, { useState } from "react";
-import { StyleSheet, Text,TextInput, Button, SafeAreaView} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  SafeAreaView,
+} from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import AuthProvider, { useAuth } from "../../AuthProvider/AuthProvider";
 
-
-
 const ForgotPasswordScreen = () => {
-    const [email, setEmail] = useState("");
-    const { resetPassword } = useAuth();
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const { resetPassword } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    async function handleLogin() {
-        try {
-            setError('');
-            setLoading(true);
-            await resetPassword(email);
-    
-        }
-        catch (err) {
-            setError('Failed to reaet password')
-            console.log(error + ":\n " + err);
-        }
-        setLoading(false);
+  async function handleForgot() {
+    try {
+      setError("");
+      setLoading(true);
+      await resetPassword(email);
+    } catch (err) {
+      setError("Failed to reaet password");
+      console.log(error + ":\n " + err);
     }
-  return (      
-<SafeAreaView style={styles.container}>
-            <Text style={styles.text}>forgot password?</Text>
-            
-            <TextInput
-              placeholder="Email"
-              keyboardType="email-address"
-              onChangeText={(text) => setEmail(text)}
-              value={email}
+    setLoading(false);
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>forgot password?</Text>
 
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
       />
-      <Button
-        title="Reset Password"
-        onClick = {resetPassword(email)}
-      />
-      </SafeAreaView>
-    )
-}
+      <Button title="Reset Password" onPress={handleForgot} />
+    </SafeAreaView>
+  );
+};
 
-export default ForgotPasswordScreen
+export default ForgotPasswordScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      marginTop: 150,
-    },
-    text: {
-      color: '#333',
-      fontSize: 24,
-      marginLeft: 25
-    },
-    
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 150,
+  },
+  text: {
+    color: "#333",
+    fontSize: 24,
+    marginLeft: 25,
+  },
+});
