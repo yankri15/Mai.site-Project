93fe1dba70c208b2d91fdd1e57ad1db9bee5d97f
Yaniv Ankri <yankri15@gmail.com>
2022-05-02 09:33:15 +0300

    Added hamburger to map screen and wanted screen

--- src/Screens/UserScreens/MapScreen.js
+++ src/Screens/UserScreens/MapScreen.js
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import ProfilePic from "../../API/ProfilePic";
 
 
-const MapScreen = () => {
+const MapScreen = ({ navigation }) => {
     return (
         <SafeAreaView>
             <Text>MapScreen</Text>
+            <ProfilePic navigation={navigation} />
         </SafeAreaView>
     )
 }
