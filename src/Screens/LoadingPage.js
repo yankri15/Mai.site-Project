01192eccd22c4195f4af79da9823863125969d94
Yoel Abecassis <yoe1997@gmail.com>
2022-05-02 18:24:59 +0300

    created the loading page

---
+++ src/Screens/LoadingPage.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LoadingPage = () => {
+  return (
+    <View>
+      <Text>LoadingPage</Text>
+    </View>
+  );
+};
+
+export default LoadingPage;
