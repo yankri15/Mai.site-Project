728e14535c88dbce0d3e60a4494ad2180961084a
Elchanan <elchanans123@gmail.com>
2022-05-04 12:39:55 +0300

    start profile

--- src/Screens/UserScreens/ProfileScreen.js
+++ src/Screens/UserScreens/ProfileScreen.js
@@ -2,124 +2,74 @@
 // need add connect to edit profile
 //for change photo(need to add button): https://www.npmjs.com/package/react-native-image-picker
 
-import {
-  View,
-  Text,
-  StyleSheet,
-  SafeAreaView,
-  Image,
-  ScrollView,
-} from "react-native";
+import React, { useState, useEffect } from "react";
+import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../../../firebase";
+import { View, Text, SafeAreaView, Image, ScrollView, } from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-import React from "react";
+import { globalStyles } from "../../styles/global";
+import { useAuth } from "../../AuthProvider/AuthProvider";
+
+
 
 const ProfileScreen = () => {
-    return (
-        <SafeAreaView style = {styles.container}>
-            <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={styles.titleBar}>
-                    <Ionicons name="ios-arrow-back" size={24} color="#52575D"></Ionicons>
-                    <Ionicons name="md-more" size={24} color="#52575D"></Ionicons>
-                </View>
-                
-                <View style={{ alignSelf: "center" }}>
-                    <View style={styles.profileImage}>
-                        <Image source={require('../../../assets/profile-pic.png')} style={styles.image} resizeMode="center"></Image>
-                    </View>
-                    <Touchable>
-                    <View style={styles.edit}>
-                        <MaterialIcons name="Edit" size={18} color="#DFD8C8"></MaterialIcons>
-                    </View>
-                    </Touchable>
-                    </View>
-                    <View style={styles.infoContainer}>
-                    <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>name - Take from user</Text>
-                    <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14 }]}>about - Take from user</Text>
-                </View>
-        <View style={styles.statsContainer}>
-          <View style={styles.statsBox}>
-            <Text style={[styles.text, { fontSize: 24 }]}>4</Text>
-            <Text style={[styles.text, styles.subText]}>Projects</Text>
+  const { currentUser } = useAuth();
+  const [name, setName] = useState("");
+  const [school, setSchool] = useState("");
+  const [neighborhood, setNeighborhood] = useState("");
+  const [organiztion, setOrganiztion] = useState("");
+  const [classs, setClasss] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+
+  useEffect(() => {
+    const getStatus = async () => {
+      if (currentUser) {
+        const docRef = doc(db, "users", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        const userData = docSnap.data()
+        setName(userData.name)
+        setSchool(userData.school)
+        setNeighborhood(userData.neighborhood)
+        setOrganiztion(userData.organiztion)
+        setClasss(userData.classs)
+        setBirthDate(userData.birthDate)
+      }
+    };
+    getStatus().catch(console.error);
+    return;
+  }, [currentUser]);
+
+  return (
+    <View style={globalStyles.profile}>
+      <View style={globalStyles.stage1}>
+        <View style={globalStyles.picAndDetails}>
+          <View>
+            <View style={globalStyles.profile_pic}>
+              <Image source={require('../../../assets/default_profile_pic.jpg')} style={globalStyles.logo_image_area} resizeMode="center"></Image>
+            </View>
           </View>
-          <View
-            style={[
-              styles.statsBox,
-              {
-                borderColor: "#DFD8C8",
-                borderLeftWidth: 1,
-                borderRightWidth: 1,
-              },
-            ]}
-          >
-            <Text style={[styles.text, { fontSize: 24 }]}>5</Text>
-            <Text style={[styles.text, styles.subText]}>intrest</Text>
+          <View>
+            <Text style={globalStyles.profile_details}>{name} {", "} {birthDate} {", "} {neighborhood}</Text>
           </View>
-          <View style={styles.statsBox}>
-            <Text style={[styles.text, { fontSize: 24 }]}>302</Text>
-            <Text style={[styles.text, styles.subText]}>????</Text>
+          <View>
+            <Text>תחומי עניין</Text>
           </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+        <View style={globalStyles.side_details}>
+          <Text style={globalStyles.circle_details}>{school}</Text>
+          <Text style={globalStyles.circle_details}>{organiztion}</Text>
+        </View>
+      </View>
+      <View style={globalStyles.stage2}>
+        <Text>המיזמים שלי</Text>
+      </View>
+      <View style={globalStyles.stage3}>
+        <Text>שתפ"ים לחיפוש</Text>
+      </View>
+    </View>
+
+
+  )
 };
 
-export default ProfileScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF",
-  },
-  text: {
-    fontFamily: "HelveticaNeue",
-    color: "#52575D",
-  },
-  image: {
-    flex: 1,
-    height: undefined,
-    width: undefined,
-  },
-  titleBar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: 24,
-    marginHorizontal: 16,
-  },
-  profileImage: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    overflow: "hidden",
-  },
-  edit: {
-    backgroundColor: "#41444B",
-    position: "absolute",
-    top: 20,
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  infoContainer: {
-    alignSelf: "center",
-    alignItems: "center",
-    marginTop: 16,
-  },
-  statsContainer: {
-    flexDirection: "row",
-    alignSelf: "center",
-    marginTop: 32,
-  },
-  statsBox: {
-    alignItems: "center",
-    flex: 1,
-  },
-  subText: {
-    fontSize: 12,
-    color: "#AEB5BC",
-    textTransform: "uppercase",
-    fontWeight: "500",
-  },
-});
+export default ProfileScreen
