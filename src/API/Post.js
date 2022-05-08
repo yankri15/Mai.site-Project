627ff65449da6b2703e1e42b8844682cbcedcc8f
Navon Sahalo <navonsahalo@gmail.com>
2022-05-08 09:30:19 +0300

    post

--- src/API/Post.js
+++ src/API/Post.js
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Image } from "react-native";
-import { db } from "../../firebase";
+import { ref } from "firebase/storage";
+import { db, storage } from "../../firebase";
 import {
     collection,
     doc,
     getDoc,
     query,
-    getDocs,
-    ref,
+    getDocs
 } from "firebase/firestore";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { async } from "@firebase/util";
+import { getDownloadURL } from "firebase/storage";
 
 const Post = ({ postID }) => {
     const [postData, setPostData] = useState("");
     const [comments, setCommnets] = useState("");
+    const [url, setUrl] = useState();
 
     useEffect(() => {
         const getpostData = async () => {
             const q = doc(db, "posts", postID);
             const docSnap = await getDoc(q);
             setPostData(docSnap.data());
+            console.log(docSnap.data().downloadURL);
+
+
+            const docRef = ref(storage, '/img/qvnWTHyygCXTOlH97zUgj8tnvq33.jpg');
+            await getDownloadURL(docRef).then((uri) => {
+                console.log(uri);
+                setUrl(uri);
+            })
         };
 
         const getComments = async () => {
@@ -27,26 +38,31 @@ const Post = ({ postID }) => {
             const qSnap = await getDocs(subCOl);
             setCommnets(qSnap.docs);
         };
+
         getpostData().catch(console.error);
-        getComments().catch(console.error);
+        //getComments().catch(console.error);
         return;
     }, []);
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View>
-                <Text>{postData.postText}</Text>
-            </View>
-            <View>
-                {console.log(postData.downloadURL)}
-                {/* {console.log(comments)} */}
-                {comments
-                    ? comments.map((item, index) => {
-                        console.log(item.data().commentText);
-                    })
-                    : console.log("shit")}
-            </View>
-        </SafeAreaView>
+        // <SafeAreaView style={{ flex: 1 }}>
+        //     <View>
+        //         <Text>{postData.postText}</Text>
+        //     </View>
+        //     <View>
+        //         {console.log(postData.downloadURL)}
+        //         {/* {console.log(comments)} */}
+        //         {comments
+        //             ? comments.map((item, index) => {
+        //                 console.log(item.data().commentText);
+        //             })
+        //             : console.log("shit")}
+        //     </View>
+        // </SafeAreaView>
+        <View>
+            <Text>{postData && postData.postText}</Text>
+            {url && <Image source={{ uri: url }} style={{ width: 200, height: 200 }} />}
+        </View>
     );
 };
 
