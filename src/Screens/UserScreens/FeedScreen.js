053a12d705909288ac62efdbf676663c3a1f24e7
Yoel Abecassis <yoe1997@gmail.com>
2022-05-08 20:39:45 +0300

    Used the PostProvider to retrive posts

--- src/Screens/UserScreens/FeedScreen.js
+++ src/Screens/UserScreens/FeedScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image, Pressable, FlatList } from "react-native";
+import { Text, Pressable, FlatList } from "react-native";
 import { globalStyles } from "../../styles/global";
 import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,29 +8,31 @@ import { getDownloadURL, ref } from "firebase/storage";
 import { db, storage } from "../../../firebase";
 import Post from "../../API/Post";
 import { collection, getDocs } from "firebase/firestore";
+import { usePost } from "../../AuthProvider/PostProvider";
 
 const FeedScreen = ({ navigation }) => {
-  const { currentUser } = useAuth();
-  const [snapshot, setSnapshot] = useState();
+  const { posts, getPostData } = usePost();
 
   useEffect(() => {
-    const getPosts = async () => {
-      const col = collection(db, "posts");
-      const colSnapshot = await getDocs(col);
-      setSnapshot(colSnapshot.docs);
-    };
-    getPosts();
+    getPostData();
   }, []);
 
+  console.log(posts);
   return (
-    <SafeAreaView style={[globalStyles.global, globalStyles.feed]}>
-      {snapshot ? (
-        <FlatList
-          data={snapshot}
-          renderItem={({ item }) => <Post navigation={navigation} style={globalStyles.list_of_posts} postID={item.id} />}
-
-        />
+    <SafeAreaView>
+      {posts && posts.length > 0 ? (
+        <Post post={posts[0]} navigation={navigation} />
       ) : (
+        // <FlatList
+        //   data={posts}
+        //   renderItem={({ item }) => (
+        //     <Post
+        //       post={item}
+        //       navigation={navigation}
+        //       style={globalStyles.list_of_posts}
+        //     />
+        //   )}
+        // />
         <Text>כרגע אין מה להציג...</Text>
       )}
 
