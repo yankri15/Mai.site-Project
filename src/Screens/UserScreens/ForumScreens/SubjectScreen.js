5695401f317e0200ed4783c6e3b6ba69969fedd2
Navon Sahalo <navonsahalo@gmail.com>
2022-05-13 18:13:10 +0300

    Forum db setup

--- src/Screens/UserScreens/ForumScreens/SubjectScreen.js
+++ src/Screens/UserScreens/ForumScreens/SubjectScreen.js
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Pressable } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState, useEffect } from 'react'
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { db } from '../../../../firebase';
 
-const SubjectScreen = () => {
+const SubjectScreen = ({ route, navigation }) => {
+    const [thread, setThread] = useState([]);
+    const topicData = route.params.item;
+    // console.log("this is the ", topicData);
+
+    useEffect(() => {
+        setThread([]);
+        const getThread = async () => {
+            const docRef = collection(db, "forum", topicData.topicId, topicData.topicName);
+            const docSnap = await getDocs(docRef);
+            // console.log(docSnap.docs[0].data());
+
+            docSnap.docs.forEach((element) => {
+                setThread((prev) => [...prev, { ...topicData, "threadId": element.id, "threadTitle": element.data().title }]);
+            })
+        }
+        if (!thread || thread.length == 0)
+            getThread().catch(console.error);
+        return;
+    }
+        , []);
+
     return (
         <SafeAreaView>
-            <Text>SubjectScreen</Text>
+            {thread && thread.length > 0 ?
+                (
+                    <FlatList
+                        data={thread}
+                        renderItem={({ item }) => (
+                            <Pressable
+                                onPress={() => navigation.navigate("Thread", { item })}>
+                                <Text>{item.threadTitle}</Text>
+                            </Pressable>
+                        )}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                ) : (
+                    <Text>מיד נציג אתכם</Text>
+                )}
         </SafeAreaView>
     )
 }
--- src/Screens/UserScreens/ForumScreens/ThreadScreen.js
+++ src/Screens/UserScreens/ForumScreens/ThreadScreen.js
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, FlatList, Pressable } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState, useEffect } from 'react'
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { db } from '../../../../firebase';
 
+const ThreadScreen = ({ route, navigation }) => {
 
-const ThreadScreen = () => {
+    const [comments, setComments] = useState([]);
+    const subjectData = route.params.item;
+
+    useEffect(() => {
+        setComments([]);
+        const getComments = async () => {
+            const docRef = collection(db, "forum", subjectData.topicId, subjectData.topicName, subjectData.threadId, "comments");
+            const docSnap = await getDocs(docRef);
+            console.log(docSnap.docs[0].data());
+
+            docSnap.docs.forEach((element) => {
+                setComments((prev) => [...prev, { ...subjectData, "commentId": element.id, "commentData": element.data() }]);
+            })
+        }
+        if (!comments || comments.length == 0)
+            getComments().catch(console.error);
+        return;
+    }
+        , []);
+
     return (
         <SafeAreaView>
-            <Text>ThreadScreen</Text>
+            {comments && comments.length > 0 ?
+                (
+                    <FlatList
+                        data={comments}
+                        renderItem={({ item }) => (
+                            <View>
+                                <Text>{item.commentData.comment}</Text>
+                                <Text>{item.commentData.uid}</Text>
+                            </View>
+                        )}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                ) : (
+                    <Text>מיד נציג אתכם</Text>
+                )}
         </SafeAreaView>
     )
 }
