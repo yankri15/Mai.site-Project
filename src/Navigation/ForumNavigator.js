e1cdd945de976719a348cabc573e8db636341d70
Yoel Abecassis <yoe1997@gmail.com>
2022-05-14 21:52:56 +0300

    navigate to profile from comment

--- src/Navigation/ForumNavigator.js
+++ src/Navigation/ForumNavigator.js
@@ -4,7 +4,7 @@ import TopicsScreen from "../Screens/UserScreens/ForumScreens/TopicsScreen";
 import SubjectScreen from "../Screens/UserScreens/ForumScreens/SubjectScreen";
 import ThreadScreen from "../Screens/UserScreens/ForumScreens/ThreadScreen";
 import CreateThread from "../Screens/UserScreens/ForumScreens/CreateThread";
-
+import ProfileScreen from "../Screens/UserScreens/ProfileScreen";
 const Stack = createStackNavigator();
 
 const ForumNavigator = () => {
@@ -14,6 +14,7 @@ const ForumNavigator = () => {
       <Stack.Screen name="Subject" component={SubjectScreen} />
       <Stack.Screen name="Thread" component={ThreadScreen} />
       <Stack.Screen name="CreateThread" component={CreateThread} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
   );
 };
