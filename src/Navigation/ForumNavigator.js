c27ecf84908fb19540fb02b6493b35642fe3e821
Yoel Abecassis <yoe1997@gmail.com>
2022-05-14 16:39:57 +0300

    added CreateThread to the stack

--- src/Navigation/ForumNavigator.js
+++ src/Navigation/ForumNavigator.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
-import TopicsScreen from '../Screens/UserScreens/ForumScreens/TopicsScreen'
-import SubjectScreen from '../Screens/UserScreens/ForumScreens/SubjectScreen'
-import ThreadScreen from '../Screens/UserScreens/ForumScreens/ThreadScreen'
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import TopicsScreen from "../Screens/UserScreens/ForumScreens/TopicsScreen";
+import SubjectScreen from "../Screens/UserScreens/ForumScreens/SubjectScreen";
+import ThreadScreen from "../Screens/UserScreens/ForumScreens/ThreadScreen";
+import CreateThread from "../Screens/UserScreens/ForumScreens/CreateThread";
 
 const Stack = createStackNavigator();
 
 const ForumNavigator = () => {
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Forum" component={TopicsScreen} />
-            <Stack.Screen name="Subject" component={SubjectScreen} />
-            <Stack.Screen name="Thread" component={ThreadScreen} />
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Forum" component={TopicsScreen} />
+      <Stack.Screen name="Subject" component={SubjectScreen} />
+      <Stack.Screen name="Thread" component={ThreadScreen} />
+      <Stack.Screen name="CreateThread" component={CreateThread} />
+    </Stack.Navigator>
+  );
+};
 
-export default ForumNavigator
+export default ForumNavigator;
