360e76abcba6330f94a0c8f89b699900b9fd3830
Navon Sahalo <navonsahalo@gmail.com>
2022-05-14 21:22:29 +0300

    Issue with profile pics fixed

--- src/API/ProfilePic.js
+++ src/API/ProfilePic.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import { View, TouchableOpacity, Image, SafeAreaView } from "react-native";
 import { globalStyles } from "../styles/global";
 import { useAuth } from "../AuthProvider/AuthProvider";
-import { db } from "../../firebase";
+import { db, storage } from "../../firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { getDownloadURL, ref } from "firebase/storage";
 
 const ProfilePic = ({ navigation }) => {
   const { currentUser } = useAuth();
@@ -14,7 +15,10 @@ const ProfilePic = ({ navigation }) => {
       const docRef = doc(db, "users", currentUser.uid);
       const docSnap = await getDoc(docRef);
       const userData = docSnap.data();
-      setImage(userData.pic);
+      const imgRef = ref(storage, userData.pic);
+      await getDownloadURL(imgRef).then((img) => {
+        setImage(img);
+      });
     };
     getUserData();
   }, []);
