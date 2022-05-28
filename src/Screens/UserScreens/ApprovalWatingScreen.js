87dab4c517e60c4e8a5534110791f9a7c11d8c8d
Yoel Abecassis <72299426+YoelAbk6@users.noreply.github.com>
2022-05-28 10:08:15 +0300

    Revert "Revert "Yoel/minor UI changes""

--- src/Screens/UserScreens/ApprovalWatingScreen.js
+++ src/Screens/UserScreens/ApprovalWatingScreen.js
@@ -1,49 +1,52 @@
-import { View, Text, Pressable, StyleSheet, Image } from 'react-native'
-import { globalStyles } from '../../styles/global'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useData } from '../../AuthProvider/UserDataProvider'
-import { useAuth } from '../../AuthProvider/AuthProvider'
+import { View, Text, Pressable, StyleSheet, Image } from "react-native";
+import { globalStyles } from "../../styles/global";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useData } from "../../AuthProvider/UserDataProvider";
+import { useAuth } from "../../AuthProvider/AuthProvider";
 
 const ApprovalWatingScreen = () => {
+  const { approveUser } = useData();
+  const { currentUser } = useAuth();
 
-    const { approveUser } = useData();
-    const { currentUser } = useAuth();
+  // function handleApprove() {
+  //     console.log("currentUser: " + currentUser.uid);
+  //     approveUser(currentUser.uid);
+  // }
 
-    function handleApprove() {
-        console.log("currentUser: " + currentUser.uid);
-        approveUser(currentUser.uid);
-    }
-
-    return (
-        <SafeAreaView style={globalStyles.container_enter_screens}>
-            <View style={globalStyles.logo}>
-                <Image source={require('../../../assets/app_icon.png')} style={globalStyles.logo_image_area} resizeMode="center"></Image>
-            </View>
-            <Text style={globalStyles.landing_title_text}>ממתין לאישור</Text>
-            <Pressable
+  return (
+    <SafeAreaView style={globalStyles.container_enter_screens}>
+      <View style={globalStyles.logo}>
+        <Image
+          source={require("../../../assets/app_icon.png")}
+          style={globalStyles.logo_image_area}
+          resizeMode="center"
+        ></Image>
+      </View>
+      <Text style={globalStyles.landing_title_text}>ממתין לאישור</Text>
+      {/* <Pressable
                 style={styles.button}
                 title="Approve"
                 onPress={handleApprove}
             >
                 <Text style={{ color: "#ffffff", fontSize: 20 }} >Approve</Text>
-            </Pressable>
-        </SafeAreaView>
-    )
-}
+            </Pressable> */}
+    </SafeAreaView>
+  );
+};
 
-export default ApprovalWatingScreen
+export default ApprovalWatingScreen;
 
 const styles = StyleSheet.create({
-    button: {
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 15,
-        marginBottom: 15,
-        paddingVertical: 10,
-        paddingHorizontal: 35,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: "#6495ED",
-    },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
+    marginBottom: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 35,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#6495ED",
+  },
 });
