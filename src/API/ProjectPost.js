7991fab16e42229deccd72d88d478f34ede34078
Yoel Abecassis <yoe1997@gmail.com>
2022-06-04 15:15:22 +0300

    Added the pid to the route

--- src/API/ProjectPost.js
+++ src/API/ProjectPost.js
@@ -1,46 +1,46 @@
-import { View, Text, Pressable } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useData } from '../AuthProvider/UserDataProvider'
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
-import { globalStyles } from '../styles/global';
+import { View, Text, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useData } from "../AuthProvider/UserDataProvider";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { globalStyles } from "../styles/global";
 
 const ProjectPost = ({ pid, navigation }) => {
+  const [project, setProject] = useState([]);
 
-    const [project, setProject] = useState([]);
+  const getProject = async (pid) => {
+    const docRef = doc(db, "projects", pid);
+    const docSnap = await getDoc(docRef);
+    setProject(docSnap.data());
+  };
 
-    const getProject = async (pid) => {
-        const docRef = doc(db, 'projects', pid);
-        const docSnap = await getDoc(docRef);
-        setProject(docSnap.data());
-    }
+  useEffect(() => {
+    getProject(pid);
+  }, []);
 
-
-    useEffect(() => {
-        getProject(pid);
-    }, [])
-
-    return (
-        <View style={globalStyles.project_details_container}>
-            <Pressable
-                style={globalStyles.project_details}
-                onPress={() => navigation.navigate("Project", { project: project })}
-            >
-                <View style={globalStyles.side_details_comp}>
-                    <Text style={globalStyles.side_details_text}>{"שם הפרויקט: "}</Text>
-                    <Text>{project.name}</Text>
-                </View>
-                <View style={globalStyles.side_details_comp}>
-                    <Text style={globalStyles.side_details_text}>{"ארגון: "}</Text>
-                    <Text>{project.organization}</Text>
-                </View>
-                <View style={globalStyles.side_details_comp}>
-                    <Text style={globalStyles.side_details_text}>{"תיאור: "}</Text>
-                    <Text>{project.description}</Text>
-                </View>
-            </Pressable>
+  return (
+    <View style={globalStyles.project_details_container}>
+      <Pressable
+        style={globalStyles.project_details}
+        onPress={() =>
+          navigation.navigate("Project", { project: project, pid: pid })
+        }
+      >
+        <View style={globalStyles.side_details_comp}>
+          <Text style={globalStyles.side_details_text}>{"שם הפרויקט: "}</Text>
+          <Text>{project.name}</Text>
         </View>
-    )
-}
+        <View style={globalStyles.side_details_comp}>
+          <Text style={globalStyles.side_details_text}>{"ארגון: "}</Text>
+          <Text>{project.organization}</Text>
+        </View>
+        <View style={globalStyles.side_details_comp}>
+          <Text style={globalStyles.side_details_text}>{"תיאור: "}</Text>
+          <Text>{project.description}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
 
-export default ProjectPost
+export default ProjectPost;
