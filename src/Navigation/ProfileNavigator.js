68c3335f856f57bb4cf5ae55a0a1d179cf4b1941
Navon Sahalo <navonsahalo@gmail.com>
2022-06-08 14:51:03 +0300

    style

--- src/Navigation/ProfileNavigator.js
+++ src/Navigation/ProfileNavigator.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 
 const ProfileNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator >
       <Stack.Screen name="פרופיל" component={ProfileScreen} />
       <Stack.Screen name="עריכת פרופיל" component={ProfileEditScreen} />
     </Stack.Navigator>
